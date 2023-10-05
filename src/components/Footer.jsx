55bc837175b3c033ad1f6b986a320e5a1161a523
Adolfo Neto <adolfo.usp@gmail.com>
2023-10-05 17:41:24 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,11 @@ export function Footer() {
           <a className='text-purple-550' href='http://elixiremfoco.com'> Elixir em Foco</a>. 
           Em caso de dúvida, envie email para elixiremfoco@gmail.com.
         </p>
+        <p className="mt-6 text-base text-slate-500 md:mt-0">
+          Este evento recebeu apoio parcial da 
+          <a className='text-purple-550' href='https://bit.ly/3Jma95g'> Erlang Ecosystem Foundation</a>. 
+          Acesse a página e se associe!.
+        </p>
       </Container>
     </footer>
   )
